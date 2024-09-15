e7ba758300252f55e747fab9f0ab961016e60b5c
syusyugenz <syusyu10x@gmail.com>
2024-09-15 19:10:20 +0700

    update
    update

--- AsciiStringTest/AsciiString.cs
+++ AsciiStringTest/AsciiString.cs
@@ -7,25 +7,27 @@ namespace AsciiStringTest
 {
     public class AsciiString
     {
+        
         private readonly char[] _buffer;
+        //readonly để ngăn cản việc gán mảng mới vào sau khi instance của AsciiString đã được khởi tạo.
+        
 
-        public AsciiString()
-        {
-            _buffer = new char[0];
+        public AsciiString(){
+            this._buffer = new char[0];
+            //tạo mảng buff với giá trị trong mảng bằng 0
         }
 
-
-        public AsciiString(params char[] chars)
-        {
-            // Sử dụng từ khóa params để cho phép truyền vào một mảng không xác định số lượng tham số
-            _buffer = new char[chars.Length];
-            Array.Copy(chars, _buffer, chars.Length);
+        public AsciiString(params char[] chars){
+            _buffer = new char[chars.Length+1];
+            // khởi tạo 1 mảng buffer mới có kí tự và độ dài bằng mảng _chars+ 1 đây là giá trị dành cho '\0'
+            Array.Copy(chars,_buffer,chars.Length);
+            // ở đây sửa dụng array.coppy để sao chép các mảng chars vào trong buffer
+            _buffer[chars.Length] = '\0';
         }
 
-        public AsciiString(string originalString)
-        {
-            // Chuyển đổi string thành mảng char
+        public AsciiString(string originalString){
             _buffer = originalString.ToCharArray();
+            //chuyen mang string thanh mang char
         }
 
         public int Capacity{
@@ -34,12 +36,15 @@ namespace AsciiStringTest
             }
         }
 
-        public int Length{
-            get{
-                int length= 0;
-                foreach(char c in _buffer)
+        public int Length
+        {
+            get
+            {
+                int length = 0;
+                foreach (char c in _buffer)
                 {
-                    if(c !='\0'){
+                    if (c != '\0')
+                    {
                         length++;
                     }
                 }
@@ -49,32 +54,46 @@ namespace AsciiStringTest
 
         public override string ToString(){
             return new string (_buffer);
+            //day la ep kieu ve string
         }
 
-        public static AsciiString operator +(AsciiString a, AsciiString b){
-            int newlengt= a.Length+ b.Length;
-            char[] newBuffer= new char[newlengt];
+        public static AsciiString Operate(AsciiString a, AsciiString b){
+            // new length= a.Length+ b.Length;
+            // char[] newBuffer= new char[length + 1];
+            int lengthA = a.Length;
+            int lengthB = b.Length;
 
-            Array.Copy(a._buffer, newBuffer, a.Length);
-            Array.Copy(b._buffer,0, newBuffer,a.Length,b.Length);
+            char[] newBuffer= new char[lengthA+ lengthB];
 
-            return new AsciiString(newBuffer);
+            Array.Copy(a._buffer, newBuffer, lengthA);
+            //dòng này là lấy các phần tử trong mảng a, và gán vào trong newBuffer
+            Array.Copy(b._buffer, 0, newBuffer,lengthA, lengthB);
+            //dòng này là lấy các phần tử trong mảng b, và gán vào trong newBuffer
+            // 0 đại diện ở vị trí 0, a._buffer là sau khi kết thúc mảng a thì mảng b sẽ ghi vào sau mảng a
+
+            return new AsciiString (newBuffer);
         }
 
         public int FindIndex(char characterToFind){
-            for(int i=0;i<_buffer.Length; i++){
+            for(int i = 0; i< _buffer.Length; i++){
                 if(_buffer [i]== characterToFind){
                     return i;
                 }
+                //_buffer [i] i đại diện cho vị trí  nếu đungs sẽ trả về vị trí của kí tự đó 
+                // ngược lại sai sẽ trả về -1;
             }
             return -1;
+
         }
-        public AsciiString ToUpper(){
-            char [] toupperBuffer= new char[_buffer.Length];
-            for(int i=0;i<_buffer.Length; i++){
-                toupperBuffer[i]=char.ToUpper(_buffer [i]);//Chuyển đổi từng ký tự thành chữ in hoa
+
+        public AsciiString ToUpper1(){
+            char [] toUper= new char[_buffer.Length];
+            for(int i = 0; i < _buffer.Length; i++){
+                toUper[i]= char.ToUpper( _buffer [i] );
+                // sử dụng toUpper() của lớp char để chuyển ký tự tại chỉ số i của _buffer thành chữ in hoa, 
+                //và gán giá trị này cho vị trí tương ứng trong toupperBuffer.
             }
-            return new AsciiString(toupperBuffer); // Trả về một instance mới của Buffer
+            return new AsciiString(toUper); // Trả về một instance mới của Buffer
         }
 
         public AsciiString ToLower(){
@@ -84,10 +103,6 @@ namespace AsciiStringTest
             }
             return new AsciiString(tolowerBuffer);
         }
-
-        
-    
-
     }
 
 }
--- AsciiStringTest/Program.cs
+++ AsciiStringTest/Program.cs
@@ -1,6 +1,12 @@
-﻿
-class Program{
-    public static void Main(string[] args){
-        
+﻿namespace AsciiStringTest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            AsciiString asc= new AsciiString('B', 'a', 'N','G');
+            AsciiString upperAscii= asc.ToUpper1();
+            Console.WriteLine("upper"+ upperAscii.ToString());
+        }
     }
 }
